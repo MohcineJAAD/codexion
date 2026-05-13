5880772480b370d4b931e86b5f86fd403ba4fd96
mohcine-jaad <mohcine.jaad@gmail.com>
2026-05-13 20:27:52 +0100

    Implement input validation and simulation initialization functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct s_simulation
@@ -19,39 +20,93 @@ int ft_is_just_nums(char *str)
 {
     int len;
     int i;
+
     i = 0;
     len = strlen(str);
+	if (len == 0)
+		return -1;
     while (i < len)
     {
-        if (str[i] < 48 || str[i] > 57)
+        if (str[i] < '0' || str[i] > '9')
             return -1;
         i++;
     }
     return 1;
 }
 
-int ft_validator(int argc, char **argv)
+void *ft_clear(int *location)
+{
+	free(location);
+	return NULL;
+}
+
+int	*ft_validator(int argc, char **argv)
 {
 	int i;
+	int value;
+	int *buffer;
 
 	i = 1;
-	while (i < argc)
+	buffer = malloc(sizeof(int) * 7);
+	if (strcmp("fifo", argv[argc - 1]) != 0 && strcmp("edf", argv[argc - 1]) != 0)
+		return ft_clear(buffer);
+	while (i < argc - 1)
 	{
 		if (ft_is_just_nums(argv[i]) == -1)
-			return -1;
+			return ft_clear(buffer);
+		else
+		{
+			buffer[i - 1] = atoi(argv[i]);
+			if (buffer[i - 1] <= 0)
+				return ft_clear(buffer);				
+		}
 		i++;
 	}
+	return buffer;
+}
+
+int ft_init_simulation(int argc, char **argv, Simulation *sm)
+{
+	int i;
+	int *data;
+
+	data = ft_validator(argc, argv);
+	if (data == NULL)
+		return -1;
+
+	i = 0;
+	sm->number_of_coders = data[i++];
+	sm->time_to_burnout = data[i++];
+	sm->time_to_compile = data[i++];
+	sm->time_to_debug = data[i++];
+	sm->time_to_refactor = data[i++];
+	sm->number_of_compiles_required = data[i++];
+	sm->dongle_cooldown = data[i];
+	sm->scheduler = argv[argc - 1];
+	free(data);
 	return 1;
 }
-// int ft_init_simulation()
 
 int main(int argc, char **argv)
 {
-    if (argc != 9)
-        printf("argiment not valid\n");
+	Simulation sm;
 
-        if (ft_validator(argc, argv) == 1)
-			printf("Simulation start\n");
-		else
-			printf("Rejected: invalid inputs\n");
+    if (argc != 9)
+    {
+		printf("argument not complete\n");
+		return 1;
+	}
+	if (ft_init_simulation(argc, argv, &sm) == 1)
+	{
+		printf("%d\n", sm.number_of_coders);
+		printf("%d\n", sm.time_to_burnout);
+		printf("%d\n", sm.time_to_compile);
+		printf("%d\n", sm.time_to_debug);
+		printf("%d\n", sm.time_to_refactor);
+		printf("%d\n", sm.number_of_compiles_required);
+		printf("%d\n", sm.dongle_cooldown);
+		printf("%s\n", sm.scheduler);
+	}
+	else
+		fprintf
 }
