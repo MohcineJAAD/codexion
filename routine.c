d37c9e1676fdf76500f5792ac9c7bb9d8a47f03c
mohcine-jaad <mohcine.jaad@gmail.com>
2026-06-04 14:09:33 +0100

    Refactor coder routine: extract compile, debug, and refactor functionalities into separate functions for improved clarity and maintainability and solve deadlock problem via brake circuler wait

--- routine.c
+++ routine.c
@@ -1,13 +1,19 @@
 #include "codexion.h"
 
-void	*coder_routine(void *coder)
+static void ft_compile(t_coder *cd, int direction)
 {
-	t_coder *cd;
-	int		i;
-
-	cd = (t_coder *)coder;
-	i = 0;
-	while (i < cd->sim->number_of_compiles_required)
+	if (direction == 1)
+	{
+		pthread_mutex_lock(&(cd->dongle_right->mutex));
+		ft_print_log(cd, "has taken a dongle\n");
+		pthread_mutex_lock(&(cd->dongle_left->mutex));
+		ft_print_log(cd, "has taken a dongle\n");
+		ft_print_log(cd, "is compiling\n");
+		usleep(cd->sim->time_to_compile * 1000);
+		pthread_mutex_unlock(&(cd->dongle_right->mutex));
+		pthread_mutex_unlock(&(cd->dongle_left->mutex));
+	}
+	else
 	{
 		pthread_mutex_lock(&(cd->dongle_left->mutex));
 		ft_print_log(cd, "has taken a dongle\n");
@@ -17,10 +23,37 @@ void	*coder_routine(void *coder)
 		usleep(cd->sim->time_to_compile * 1000);
 		pthread_mutex_unlock(&(cd->dongle_left->mutex));
 		pthread_mutex_unlock(&(cd->dongle_right->mutex));
-		ft_print_log(cd, "is debugging\n");
-		usleep(cd->sim->time_to_debug * 1000);
-		ft_print_log(cd, "is refactoring\n");
-		usleep(cd->sim->time_to_refactor * 1000);
+	}
+	
+}
+
+static void	ft_debugging(t_coder *cd)
+{
+	ft_print_log(cd, "is debugging\n");
+	usleep(cd->sim->time_to_debug * 1000);
+}
+
+static void ft_refactoring(t_coder *cd)
+{
+	ft_print_log(cd, "is refactoring\n");
+	usleep(cd->sim->time_to_refactor * 1000);
+}
+
+void	*coder_routine(void *coder)
+{
+	t_coder *cd;
+	int		i;
+
+	cd = (t_coder *)coder;
+	i = 0;
+	while (i < cd->sim->number_of_compiles_required)
+	{
+		if (cd->id == cd->sim->number_of_coders)
+			ft_compile(cd, 1);
+		else
+			ft_compile(cd, 0);
+		ft_debugging(cd);
+		ft_refactoring(cd);
 		i++;
 	}
 	return NULL;
